133ed448d91de9d243621d6dfcc9d5961bdfe574
Emircan Kirez <emircankirezz@gmail.com>
2023-08-01 15:50:47 +0300

    add dagger-hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -52,4 +54,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // dagger-hilt
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/emircankirez/yummy/YummyApp.kt
@@ -0,0 +1,8 @@
+package com.emircankirez.yummy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YummyApp : Application() {
+}
