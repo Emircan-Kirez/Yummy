94a5a333561c2bb339c0104e670c9d986f020089
Emircan Kirez <emircankirezz@gmail.com>
2023-08-15 18:48:32 +0300

    fix status bar

--- app/src/main/java/com/emircankirez/yummy/ui/MainActivity.kt
+++ app/src/main/java/com/emircankirez/yummy/ui/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 else -> {
-                    hideStatusBar()
+                    showStatusBar()
                     bottomNavigationView.hide()
                 }
             }
